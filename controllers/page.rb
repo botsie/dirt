f207d349c5809944b90d7f0057cd63901b4566a7
Biju Chacko <biju.chacko@gmail.com>
2012-11-21 19:18:06 +0530

    Clicking on a non-existant wikilink will prompt you to create it.

--- controllers/page.rb
+++ controllers/page.rb
@@ -5,20 +5,39 @@ module Dirt
     def show(params)
       @project = params[:project]
       @page_name = params[:page]
-      @page = Dirt::Page.html(@project, @page_name)
-      haml :page
+      begin
+        @page = Dirt::Page.html(@project, @page_name)
+      rescue RuntimeError => e
+        raise unless e.message == "Page Not Found"
+        edit(params)
+      else
+        haml :page
+      end
     end
 
     def edit(params)
       @project = params[:project]
       @page_name = params[:page]
-      @page = Dirt::Page.source(@project, @page_name)
+      begin
+        @page = Dirt::Page.source(@project, @page_name)
+      rescue RuntimeError => e
+        raise unless e.message == "Page Not Found"
+        @page = {:content => ""}
+      end
       haml :page_edit
     end
 
     def save(params)
       # TODO: escape content to prevent SQL injection
-      Dirt::Page.where(:id => params[:id]).update(:content => params[:content])
+
+      Dirt::Page.persist(
+        :id => params[:id],
+        :page_name => params[:page_name],
+        :project_id => params[:project_id],
+        :content => params[:content],
+        :project => params[:project]
+      )
+
       show(params)
     end 
   end
--- models/page.rb
+++ models/page.rb
@@ -12,6 +12,7 @@ module Dirt
 
     def self.source(project, page_name)
     	page = self.eager_graph(:project).where(:pages__name => page_name, :project__identifier => project).first
+      raise "Page Not Found" if page.nil?
     	page
     end	
 
@@ -22,6 +23,15 @@ module Dirt
 
 	    page[:html] = RedCloth.new(pre_processed_src).to_html
 	    page
+    end
+
+    def self.persist(args = {})
+      if args[:id].empty?
+        project_id = Dirt::Project.where(:identifier => args[:project]).first.id
+        self.insert(:name => args[:page_name], :project_id => project_id, :content => args[:content])
+      else
+        self.where(:id => args[:id]).update(:content => args[:content])
+      end
     end
 
     def self.expand_macros(text)
