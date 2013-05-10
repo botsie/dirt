50ee2f3b3063a4001e1e2d84b29ee891bc7a99dd
vignesh <vignesh@localhost.localdomain>
2013-05-10 13:11:25 +0530

    moved css and js files and added knockout lib to repo

---
+++ public/js/taskboard.js
@@ -0,0 +1,10 @@
+/*
+ * taskboard.js - should be loaded while rendering taskboard for each project
+ * Uses Knockout.js to monitor changes in DOM
+ *
+ */
+
+(function(window, document, ko, undefined){
+	console.log("loaded taskboard.js");
+	
+})(window, document, ko);
