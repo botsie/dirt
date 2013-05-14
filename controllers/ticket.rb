1c44f3599231e202c0f4a51392a29d277e50da9a
nobelium <me@vignesh.info>
2013-05-14 15:31:18 +0530

    adding search and ticket controller

---
+++ controllers/ticket.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+module Dirt
+
+  class TicketController < Dirt::Controller
+    def self.process(params, session)
+    end
+
+    def addComment
+    end
+
+    def delComment
+    end
+
+    def editComment
+    end
+
+    def editTicket
+      #edit can change status, ticket name, description etc
+    end
+
+    def getComment
+      #do long poll - have to think abt it.
+    end
+  end
+end
