db5dfdd5491b1bff28ba3ec472f51e309857bb9b
nobelium <me@vignesh.info>
2013-05-22 11:14:59 +0530

    added gradient for pie chart

--- public/js/piechart.js
+++ public/js/piechart.js
@@ -1,36 +1,49 @@
 $(function () {
-    $('#container').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: 'some text'
-        },
-        tooltip: {
-    	    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-        	percentageDecimals: 1
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    color: '#000000',
-                    connectorColor: '#000000',
-                    formatter: function() {
-                        return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(3) +' %';
+        
+        // Radialize the colors
+        Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function(color) {
+            return {
+                radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+                stops: [
+                    [0, color],
+                    [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                ]
+            };
+        });
+        
+        // Build the chart
+        $('#container').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: 'some text'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+                percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(3) +' %';
+                        }
                     }
                 }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: caption,
-            data: graphdata
-        }]
+            },
+            series: [{
+                type: 'pie',
+                name: caption,
+                data: graphdata
+            }]
+        });
     });
-});
     
