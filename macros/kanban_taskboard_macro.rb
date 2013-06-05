b20c3d4f5ef47a70d5de3d91115132b99c4a3d20
nobelium <me@vignesh.info>
2013-06-05 15:54:04 +0530

    working on sql

--- macros/kanban_taskboard_macro.rb
+++ macros/kanban_taskboard_macro.rb
@@ -47,6 +47,73 @@ module Dirt
       end
     end
 
+    def classify(time)
+      case Date.today - time.to_date
+      when 0..7
+        return "this-week"
+      when 8..30
+        return "this-month"
+      else
+        return "old"
+      end
+    end
+
+    def cards()
+      return @cards unless @cards.nil?
+
+      # Select cards from RT 
+      # Select cards from dirt
+      # merge both
+
+      # Note: if both are mysql databases linked server can be created and the two queries can be combined
+
+
+      sql = "SELECT 
+                et.id AS id,
+                et.Subject AS Subject,
+                et.Owner AS Owner,
+                et.LastUpdated AS LastUpdated,
+                et.Created AS Created,
+                l.LocalTarget AS Parent 
+            FROM expanded_tickets et
+            LEFT JOIN Links l ON et.id = l.LocalBase AND l.Type = 'MemberOf'
+            WHERE et.#{lane_column()} IN('#{lanes.join("', '")}')
+              AND ((Resolved BETWEEN '#{first_date}' AND '#{last_date}') OR Status <> 'resolved')"
+
+      if stream_ids.empty?
+        sql <<  "AND et.Queue IN('#{queues.join("', '")}')"
+      else
+        sql <<  "AND (l.LocalTarget IN(#{stream_ids.join(', ')}) 
+                    OR et.Queue IN('#{queues.join("', '")}'))"
+      end
+      sql << " ORDER BY Parent DESC"
+
+      ds = Dirt::RT_DB[sql]
+
+      raw_cards = ds.all
+
+      card_ids = Array.new()
+      card_list = ""
+
+      @cards = raw_cards.collect do |ticket|
+        card_ids.push(ticket[:id])
+        card_list = card_list + "'#{ticket[:id]}' "
+        ticket[:short_subject] = shorten(ticket[:Subject])
+        ticket[:age_class] = classify(ticket[:LastUpdated])
+        ticket
+      end
+
+      # have the id's of the cards, we can get the kanban status from dirt db
+      # sql = "SELECT * FROM `table_status` AS `ts` LEFT JOIN `statuses` AS `s` ON `ts`.`status_id`=`s`.`id` WHERE `ticket_id` in (#{card_list})"
+      # Have find how to do the above sql query using Dirt::DIRT_DB
+
+
+      # In Rails activerecord 
+      TicketStatus.where(:ticket_id => card_ids).joins("LEFT JOIN statuses on `statuses`.`id` = `ticket_status`.`id`")
+      # have to do it for sequel
+
+
+    end
    
   end
 
