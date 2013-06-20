90978ced9b85085e7837644c5f0e3b346c44671f
nobelium <me@vignesh.info>
2013-06-20 17:18:23 +0530

    bug fix for status map

--- controllers/project.rb
+++ controllers/project.rb
@@ -33,9 +33,46 @@ module Dirt
         :tab_spec => params[:tab_spec]
         )
 
-      params[:statuses].each do |status|
-        if !status["kanban"].empty?
-          Dirt::Status.persist(:project_id => params[:id], :status_name => status["kanban"] , :rt_status_id => status["RT"])
+      oldstatuses = Dirt::Status.where(:project_id => params[:id]).all
+
+      deleted_status = Array.new
+
+      #delete all oldstatuses that are not in the new list
+      if !oldstatuses.nil?
+        oldstatuses.each do |oldstatus|
+          flag = 0 
+          if !params[:statuses].nil?
+            params[:statuses].each do |newstatus|
+              if oldstatus[:status_name].to_s == newstatus["kanban"].to_s
+                flag = 1
+                break
+              end
+            end
+          end
+          if(flag==0)
+            Dirt::Status.where(:id => oldstatus[:id]).delete
+            #move all tickets with the above status id to uncategorized
+            Dirt::StatusTicket.where(:status_id => oldstatus[:id]).update(:status_id => '0')
+          end
+        end
+      end
+      
+      if !params[:statuses].nil?
+        params[:statuses].each do |newstatus|
+          #update all old statuses with new rt_map id
+          #if updated - do not insert
+          flag = 0
+          if !oldstatuses.nil?
+            oldstatuses.each do |oldstatus|
+              if oldstatus[:status_name] == newstatus["kanban"] && oldstatus[:rt_status_id].to_i != newstatus["RT"].to_i
+                Dirt::Status.where(:id => oldstatus[:id]).update(:rt_status_id => newstatus["RT"])
+                flag = 1
+              end
+            end
+            if !newstatus["kanban"].empty? && flag==0
+              Dirt::Status.persist(:project_id => params[:id], :status_name => newstatus["kanban"] , :rt_status_id => newstatus["RT"])
+            end
+          end
         end
       end
 
